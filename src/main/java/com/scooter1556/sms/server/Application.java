7ea806172854c814dd1d1f09d8c7c1c5e8aa7cf2
Scott Ware <scott2.ware@gmail.com>
2019-03-23 21:11:21 +0000

    Spring: Update Spring Boot to v2.1.3
    Signed-off-by: Scott Ware <scott2.ware@gmail.com>

--- src/main/java/com/scooter1556/sms/server/Application.java
+++ src/main/java/com/scooter1556/sms/server/Application.java
@@ -27,7 +27,7 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
--- src/main/java/com/scooter1556/sms/server/config/SecurityConfig.java
+++ src/main/java/com/scooter1556/sms/server/config/SecurityConfig.java
@@ -27,7 +27,7 @@ import com.scooter1556.sms.server.database.UserDatabase;
 import com.scooter1556.sms.server.utilities.DatabaseUtils;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
